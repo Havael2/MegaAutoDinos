ea087fbca39dd68c7f7c6e7441ba18539eb9dc04
gustaamelo <162834354+gustaamelo@users.noreply.github.com>
2024-04-03 19:54:25 -0300

    megaAutoDinos.c

--- megaAutoDinos.c
+++ megaAutoDinos.c
@@ -7,6 +7,7 @@
 #define TAMadj 11 // Deve estar de acordo com a quantidade de adjetivos já feitos. TAMadj será 11 ao final.
 #define TAMsubs 11 // Deve estar de acordo com a quantidade de substantivos já feitos. TAMsubs será 11 ao final.
 
+dinos especies(int spc) {
 dinos bicho;
     if (spc == 0) {
         strcpy(bicho.nome, "Tiranossauro Rex");

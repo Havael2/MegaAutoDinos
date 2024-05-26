c494b864655ec186ed3a10b7243c5fb5e7a18d3e
gustaamelo <162834354+gustaamelo@users.noreply.github.com>
2024-05-25 21:27:21 -0300

    Update titulo.h

--- source/include/titulo.h
+++ source/include/titulo.h
@@ -2,11 +2,20 @@
 #define TITULO_H
 
 #include <stdio.h>
+#include <windows.h>
 
 
 void titulo(){
 
 
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+  WORD saved_attributes;
+  
+   GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+   saved_attributes = consoleInfo.wAttributes;
+  
+  SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
 printf("##   ##  #######   #####     ###        ###    ##   ##   # #####  #####      #####    ######  ##   ##   #####    #####  \n");
 printf("### ###   ##   #  ##   ##   ## ##      ## ##   ##   ##  ## ## ## ### ###      ## ##     ##    ###  ##  ### ###  ##   ## \n");
 printf("#######   ##      ##       ##   ##    ##   ##  ##   ##     ##    ##   ##      ##  ##    ##    #### ##  ##   ##  ##      \n");
@@ -15,12 +24,16 @@ printf("##   ##   ##      ##   ##  #######    #######  ##   ##     ##    ##   ##
 printf("##   ##   ##   #  ##   ##  ##   ##    ##   ##  ##   ##     ##    ### ###      ## ##     ##    ##  ###  ### ###  ##   ## \n");
 printf("### ###  #######   #####   ##   ##    ##   ##   #####     ####    #####      #####    ######  ##   ##   #####    #####  \n");
 
+SetConsoleTextAttribute(hConsole, saved_attributes);
+
 
 }
 
 
 
 #endif
+	
+	
 	
 	
 	

85b845bcc81c202a1e31d32343b67004a5c0167b
jh1409 <104837495+jh1409@users.noreply.github.com>
2024-06-19 16:57:21 -0300

    Update loja.h

--- source/include/loja.h
+++ source/include/loja.h
@@ -3,25 +3,26 @@
 
 #include "listadinos.h"
 
-void loja(tp_lista_dinos **dgrupo) {
+void loja(tp_lista_dinos *dgrupo) {
     tp_lista_dinos *loja;
     loja = criar_lista();
     int dinheiro = 10;
     char opcao;
 
-    embaralharGrupo(&loja, 4);
+    embaralharGrupo(loja, 4);
     while (dinheiro >= 0) {
         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         printf("\n--Loja--\n");
         imprimir_lista(loja);
         printf("\n--Time--\n");
-        imprimir_lista(*dgrupo);
+        imprimir_lista(dgrupo);
 
         printf("\nDinheiro - R$%d\n\n", dinheiro);
         printf("c - Efetuar uma compra na loja (cada dinossauro custa R$3)\n");
         printf("v - Realizar uma venda para a loja\n");
         printf("a - Atualizar a loja (custa R$1)\n");
         printf("t - Trocar posicao de dois dinossauros no time\n");
+        printf("f - Escolher uma comida(custa R$2)\n");
         printf("Pressione qualquer outra tecla para iniciar a batalha\n");
         printf("\nEscolha a sua opcao: ");
         scanf(" %c", &opcao);
@@ -30,7 +31,7 @@ void loja(tp_lista_dinos **dgrupo) {
 
             case 'c':
                 if (dinheiro >= 3) {
-                    compra(&loja, dgrupo);
+                    compra(loja, dgrupo);
                     dinheiro -= 3;
                 } else {
                     printf("DINHEIRO INSUFICIENTE\n");
@@ -44,8 +45,8 @@ void loja(tp_lista_dinos **dgrupo) {
 
             case 'a':
                 if (dinheiro >= 1) {
-                    destruir_lista(&loja);
-                    embaralharGrupo(&loja, 4);
+                    destruir_lista(loja);
+                    embaralharGrupo(loja, 4);
                     dinheiro--;
                 }
                 break;
@@ -54,6 +55,11 @@ void loja(tp_lista_dinos **dgrupo) {
                 trocar_posicao(dgrupo);
                 break;
 
+			case 'f':
+				comida(dgrupo);
+				dinheiro-=2;
+				break;
+			
             default:
                 return;
                 break;

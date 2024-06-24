ce8a7d3dfefb3305e758763ffed5666fa3d1ff4e
gustaamelo <162834354+gustaamelo@users.noreply.github.com>
2024-06-24 18:38:45 -0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,36 +1,42 @@
+
 #include <time.h>
 #include "dinos.h"
 #include "nomegrupo.h"
-#include "habilidades.h"
 #include "listadinos.h"
 #include "filadinos.h"
 #include "batalha.h"
 #include "loja.h"
 #include "titulo.h"
 
-//testes
+
 int main() {
     srand(time(NULL));
-    int coracao=5,turno=1;
+    int coracao= 5,turno=1, trofeu= 0;
     char nomeGrupo[40];
     tp_lista_dinos *grupo;
-    FilaSE *clone;
+    FilaSE *clone, *bot;
 
     titulo();
     grupo = criar_lista();
+    clone = criar_filase();
+    bot = criar_filase();
     definir_nome(nomeGrupo);
 
-    while(coracao){
-        loja(grupo);
-
-        clone = lisFil(grupo);
-        coracao-=batalha(clone, nomeGrupo);
-        turno++;
-        if(turno%3==0){
-            coracao++;
-        }
+    while (coracao) {
+      loja(grupo);
+      clone = transferir_lista_para_fila(grupo, clone);
+      coracao-=batalha(clone, nomeGrupo);
+      if (coracao == 1){
+      coracao--;
+     }
+      else if (coracao == -1){
+      trofeu++;
+     }
+      turno++;
+      if(turno%3==0){
+      coracao++;
     }
-
+  }
     return 0;
 }
 
